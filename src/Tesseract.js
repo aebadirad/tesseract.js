6f4aa41ea92bcb93ee2ae468e3b11db4652159d8
Alex Ebadirad <alex@ebadirad.com>
2015-10-05 17:33:58 -0700

    Objectification of the package, plus allow for setting paths to workerjs and tessdata

--- src/Tesseract.js
+++ src/Tesseract.js
@@ -1,22 +1,47 @@
-var Tesseract = (function(){
-
-	var Tesseract = {}
+var Tesseract = function () {};
+	Tesseract = {
 
 	//https://cdn.rawgit.com/naptha/tesseract.js/master/worker/worker.js
 	//https://rawgit.com/naptha/tesseract.js/master/worker/worker.js   for testing
 	//https://cdn.rawgit.com/naptha/tesseract.js/master/worker/worker.js
+	//Where am I? Let's find out!
+	root : window.location.protocol + '//' + window.location.host,
+	
+	//set as relative path to root if you want a local tessdata, starting and trailing slash required
+	//example /data/Tessdata/
+	tessdataPath : false,
+	//set as relative path to root if you want a local worker file rather than the most recent, starting and trailing slash required
+	//example /js/tesseractjs/worker.js
+	workerJSPath : false,
+	bigworker: false,
+	worker : false,
+	index : 0,
+	handlers : [],
+	
+	//initialize the base object
+	init: function(options){
+	var self = this;
+	
+	//Check for options object
+	if(options){
+		this.setPaths(options);
+	}
+	
+	//Case check for local vs. remote 
+	var workerPath;
+	if(this.workerJSPath){
+	workerPath = this.root+this.workerJSPath;
+	}else{
+	workerPath = "https://cdn.rawgit.com/naptha/tesseract.js/master/lib/worker.2015.07.26.js"; // changed on build
+	}
+	var blob = new Blob(["importScripts('"+workerPath+"');"]);
+	this.worker = new Worker(window.URL.createObjectURL(blob));
+	this.worker.postMessage({init: {mem: 16777216*6}, rootURL : this.root, localTessdata: this.tessdataPath })
 
-	var blob = new Blob(["importScripts('__worker__');"]); // changed on build
-	// console.log('localhost')
-	var worker = new Worker(window.URL.createObjectURL(blob));
-	worker.postMessage({init: {mem: 16777216*6}})
-	var bigworker = false
 
-	var index = 0
-	var handlers = []
 
-	worker.onmessage = function(e){
-		var handler = handlers[e.data.index]
+	this.worker.onmessage = function(e){
+		var handler = self.handlers[e.data.index]
 		if(e.data.progress){
 			handler.progress(e.data.progress)
 		}
@@ -29,8 +54,10 @@ var Tesseract = (function(){
 			handler.callback(null,e.data.result)
 		}
 	}
+},
 
-	function convertToImageData(image){
+
+	 convertToImageData: function(image){
 		if(image.getContext){
 			image = image.getContext('2d');
 		}else if(image.tagName == "IMG" || image.tagName == "VIDEO"){
@@ -38,6 +65,10 @@ var Tesseract = (function(){
 			if(image.tagName == "IMG"){
 				c.width  = image.naturalWidth;
 				c.height = image.naturalHeight;
+				if(c.width === 0){
+					c.width = image.width;
+					c.height = image.height;
+				}
 			}else if(image.tagName == "VIDEO"){
 				c.width  = image.videoWidth;
 				c.height = image.videoHeight;
@@ -46,12 +77,24 @@ var Tesseract = (function(){
 			ctx.drawImage(image, 0, 0);
 			image = ctx;
 		}
-		if(image.getImageData) image = image.getImageData(0, 0, image.canvas.width, image.canvas.height);
+		
+		if(image.getImageData) image = image.getImageData(0, 0, Math.round(image.canvas.width), Math.round(image.canvas.height));
 		return image	
-	}
-
-	Tesseract.detect = function(image, progress, callback){
-		image = convertToImageData(image)
+	},
+	
+	setPaths : function(paths) {
+		if(paths.tessdataPath){
+		this.tessdataPath = paths.tessdataPath;
+		}
+		if(paths.workerJSPath){
+		this.workerJSPath = paths.workerJSPath;
+		}
+		
+	},
+	
+	detect : function(image, progress, callback){
+		var self = this;
+		image = this.convertToImageData(image)
 
 		if(typeof progress === "undefined"){
 			progress = callback = new Function()
@@ -62,32 +105,33 @@ var Tesseract = (function(){
 			progress = new Function()
 		}
 
-		var i = index++
+		var i = this.index++
 
-		handlers[i] = {
+		this.handlers[i] = {
 			resolve: new Function(),
 			reject: new Function()
 		}
-		handlers[i].callback = callback
-		handlers[i].progress = progress
+		this.handlers[i].callback = callback
+		this.handlers[i].progress = progress
 		
 		return new Promise(function(resolve, reject){
-			handlers[i].resolve = resolve
-			handlers[i].reject = reject
-			worker.postMessage({index: i, fun: 'detect', image: image})
+			self.handlers[i].resolve = resolve
+			self.handlers[i].reject = reject
+			self.worker.postMessage({index: i, fun: 'detect', image: image})
 		})
 
-	}
+	},
 
-	Tesseract.recognize = function(image, options, callback){
+	recognize: function(image, options, callback){
 		var lang = options.lang
+		var self = this;
 		if (typeof lang === "undefined"){
 			lang = 'eng'
 		}
 
-		if (!bigworker && ['chi_sim', 'chi_tra', 'jpn'].indexOf(lang) != -1){
-			worker.postMessage({init: {mem: 16777216*10}})
-			bigworker = true
+		if (!this.bigworker && ['chi_sim', 'chi_tra', 'jpn'].indexOf(lang) != -1){
+			this.worker.postMessage({init: {mem: 16777216*10}})
+			this.bigworker = true
 			console.log('started big worker')
 		}
 
@@ -100,17 +144,17 @@ var Tesseract = (function(){
 			callback = options
 			options = {}
 		}
+		image = this.convertToImageData(image)
 
-		image = convertToImageData(image)
 
-		var i = index++
+		var i = this.index++
 
-		handlers[i] = {
+		this.handlers[i] = {
 			resolve: new Function(),
 			reject: new Function()
 		}
-		handlers[i].callback = callback || new Function()
-		handlers[i].progress = (function(){
+		this.handlers[i].callback = callback || new Function()
+		this.handlers[i].progress = (function(){
 			if(typeof options.progress === 'function'){
 				var p = options.progress
 				delete options.progress
@@ -120,11 +164,11 @@ var Tesseract = (function(){
 		})()
 
 		return new Promise(function(resolve, reject){
-			handlers[i].resolve = resolve
-			handlers[i].reject = reject
-			worker.postMessage({index: i, fun: 'recognize', image: image, lang: lang, options: options})
+			self.handlers[i].resolve = resolve
+			self.handlers[i].reject = reject
+			var messageW = {index: i, fun: 'recognize', image: image, lang: lang, options: {lang: lang}};
+			self.worker.postMessage(messageW);
 		})
 	
 	}
-	return Tesseract
-})()
+};
